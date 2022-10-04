b3728768bac4abcdee350c8dcebe71114c6df23b
Davis Toler <tolerdavis0@gmail.com>
2022-10-04 12:52:11 -0500

    commit tree 1f2f89d67656e44a92ef7ad597adcb5c3bd5ff7c

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution in this file!
+const employee = {
+    name: "Joe",
+    streetAddress: "11 code s",
+}
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newEmployee = {...obj}
+    newEmployee[key] = value
+
+    return newEmployee 
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+
+    return obj 
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const deleteEmployee = {...obj} 
+
+    delete deleteEmployee[key]
+
+    return deleteEmployee
+
+
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key]
+
+    return obj 
+}
+ 
+
+
+
+
+
+    
+
+
+
+
+
+
